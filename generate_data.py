6df6c0db2609c8b4541b00fe0bf5a5451bcf1f1b
pdasigi <pradeepd@allenai.org>
2020-04-23 14:51:14 -0700

    working seq2seq config and updated requirements

--- generate_data.py
+++ generate_data.py
@@ -1,5 +1,7 @@
 import random
 import math
+import argparse
+from typing import List, Dict
 
 class DataGenerator:
     """
@@ -27,7 +29,9 @@ class DataGenerator:
         ]
 
 
-    def generate_expression(self, num_operations, allowed_operators=None):
+    def generate_expression(self,
+                            num_operations: int,
+                            allowed_operators: List[Dict] = None):
         """
         Generates a single expression that contains the given number of operations.
         """
@@ -48,19 +52,21 @@ class DataGenerator:
                                                   allowed_operators[operator_index:])
         second_argument = self.generate_expression(num_operations_for_second,
                                                    allowed_operators[operator_index:])
-        return {"meaning": operator["meaning"] + first_argument["meaning"] + second_argument["meaning"],
+        meaning_representation = operator["meaning"] + first_argument["meaning"] + second_argument["meaning"]
+        meaning_representation = ['('] + meaning_representation + [')']
+        return {"meaning": meaning_representation,
                 "translation": " ".join([first_argument["translation"], operator["translation"],
                                          second_argument["translation"]]),
                 "denotation": operator["denotation"](first_argument["denotation"], second_argument["denotation"])}
 
 
     def generate_data(self,
-                      num_expressions,
-                      min_num_operations=1,
-                      max_num_operations=10,
-                      split_data=False,
-                      train_proportion=0.8,
-                      test_proportion=0.1):
+                      num_expressions: int,
+                      min_num_operations: int = 1,
+                      max_num_operations: int = 10,
+                      split_data: bool = False,
+                      train_proportion: float = 0.8,
+                      test_proportion: float = 0.1):
         """
         Returns ``num_expressions`` expressions, containing number of operations in the range
         ``(min_num_operations, max_num_operations)``. Optionally, you can also have the data split into
@@ -85,3 +91,63 @@ class DataGenerator:
         return {"test": data[:test_size],
                 "dev": data[test_size:test_size+dev_size],
                 "train": data[test_size+dev_size:]}
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num-expressions', type=int, required=True, dest="num_expressions",
+                        help="Number of expressions to generate")
+    parser.add_argument('--min-num-operations', type=int, dest="min_num_operations", default=1)
+    parser.add_argument('--max-num-operations', type=int, dest="max_num_operations", default=10)
+    parser.add_argument('--output', type=str, required=True,
+                        help="""Location where output will be written. If splitting data, the name of the split
+                                will be appended to the file name""")
+    parser.add_argument('--split-data', action="store_true", dest="split")
+    parser.add_argument('--train-proportion', type=float, dest="train_proportion",
+                        help='How big should the train split be? (Between 0 and 1)')
+    parser.add_argument('--test-proportion', type=float, dest="test_proportion",
+                        help="""How big should the test split be? (Between 0 and 1). Will also make a dev split
+                                if train_proportion + test_proportion < 1""")
+    parser.add_argument('--no-meaning', action="store_true", dest="no_meaning",
+                        help="Generated data will have denotations instead of meaning")
+    args = parser.parse_args()
+    if args.no_meaning:
+        raise NotImplementedError
+    data_generator = DataGenerator()
+    data = data_generator.generate_data(num_expressions=args.num_expressions,
+                                        min_num_operations=args.min_num_operations,
+                                        max_num_operations=args.max_num_operations,
+                                        split_data=args.split,
+                                        train_proportion=args.train_proportion,
+                                        test_proportion=args.test_proportion)
+    if args.split:
+        filename_parts = args.output.split(".")
+        assert len(filename_parts) == 2, "Cannot decide how to alter the file name. Expected just one ."
+        train_file_name = f"{filename_parts[0]}_train.{filename_parts[1]}"
+        dev_file_name = f"{filename_parts[0]}_dev.{filename_parts[1]}"
+        test_file_name = f"{filename_parts[0]}_test.{filename_parts[1]}"
+        with open(train_file_name, "w") as output_file:
+            for datum in data["train"]:
+                source = datum["translation"]
+                target = " ".join(datum["meaning"])
+                print(f"{source}\t{target}", file=output_file)
+        with open(dev_file_name, "w") as output_file:
+            for datum in data["dev"]:
+                source = datum["translation"]
+                target = " ".join(datum["meaning"])
+                print(f"{source}\t{target}", file=output_file)
+        with open(test_file_name, "w") as output_file:
+            for datum in data["test"]:
+                source = datum["translation"]
+                target = " ".join(datum["meaning"])
+                print(f"{source}\t{target}", file=output_file)
+    else:
+        with open(args.output, "w") as output_file:
+            for datum in data["data"]:
+                source = datum["translation"]
+                target = " ".join(datum["meaning"])
+                print(f"{source}\t{target}", file=output_file)
+
+
+if __name__ == "__main__":
+    main()
